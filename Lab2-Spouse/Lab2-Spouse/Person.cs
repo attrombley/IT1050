d7b67d1ab7969d6a799b1585fa08314693b09a0c
attrombley <atrombom@gmail.com>
2016-09-21 21:33:44 -0400

    Start of Lab2 Ideas

---
+++ Lab2-Spouse/Lab2-Spouse/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2_Spouse
+{
+    class Person
+    {
+        public int Age;
+        public string FirstName;
+        public string LastName;
+        public Person Spouse;
+
+        public string GetFullNName()
+        {
+            return FirstName + " " + LastName;
+        }
+        public void PrintNameAndAge()
+        {
+            Console.WriteLine(LastName + "' " + FirstName + " " + "(" + Age + ")");
+        }
+    }
+}
--- Lab2-Spouse/Lab2-Spouse/Program.cs
+++ Lab2-Spouse/Lab2-Spouse/Program.cs
@@ -6,10 +6,38 @@ using System.Threading.Tasks;
 
 namespace Lab2_Spouse
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-        }
-    }
+            Person p1 = new Person();
+
+            Console.WriteLine("What is your Age?");
+            p1.Age = Console.Parse();
+
+            Console.WriteLine("What is your Age?");
+            p1.FirstName = Console.ReadLine();
+
+            Console.WriteLine("What is your Spouse's last name?");
+            p1.LastName = Console.ReadLine();
+
+            p1.Spouse = new Person();
+
+            Console.WriteLine("What is your Spouse's age?");
+            p1.Spouse.Age = Console.ReadLine();
+
+            Console.WriteLine("What is your Age?");
+            p1.Spouse.FristName = Console.ReadLine();
+
+            p1.Spouse.LastName = p1.LastName;
+            p1.Spouse.Spouse = p1;
+
+
+
+
+            Person p2 = new Person();
+
+            public static double SumOfAllAges();
+           }
+     }
 }
